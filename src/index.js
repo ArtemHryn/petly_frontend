feab21329b966dd292fe3706f8cb49504174474c
ArtemHryn <grynbest@gmail.com>
2023-02-13 00:36:47 +0200

    browser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 import 'normalize.css';
@@ -9,7 +10,9 @@ import { ThemeProvider } from 'styled-components';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/petly_frontend">
         <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
