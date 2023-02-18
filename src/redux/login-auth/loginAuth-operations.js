bf107b6a0041dab07371c938d2a8f4c0616d2b2c
NazarBezpaliuk <nazardxvx@gmail.com>
2023-02-18 14:05:08 +0200

    login-redux

---
+++ src/redux/login-auth/loginAuth-operations.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://arcane-everglades-20653.herokuapp.com';
+
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+export const logIn = createAsyncThunk(
+  'auth/login',
+  async (loginData, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/users/login', loginData);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
