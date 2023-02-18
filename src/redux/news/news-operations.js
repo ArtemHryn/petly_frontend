5cf955d1825768233a017314da95c889a1cc9182
Artem <grynbest@gmail.com>
2023-02-18 21:53:46 +0200

    Update news-operations.js

--- src/redux/news/news-operations.js
+++ src/redux/news/news-operations.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://arcane-everglades-20653.herokuapp.com';
-
 export const fetchNews = createAsyncThunk(
   "news/fetchNews",
   async (_, thunkAPI) => {
