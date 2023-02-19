5b1f2d1684beaccf79d176d87d8dc83a21a40b8a
Kseniia30 <kellyshikova3004@gmail.com>
2023-02-20 00:01:47 +0200

    change

--- src/redux/userPage/userOperations.js
+++ src/redux/userPage/userOperations.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.headers.common.Authorization = `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2YwYWQ0ZGY0MjZlY2UyYTJiNWViMzkiLCJlbWFpbCI6ImtlbGx5c2hpa292YTMwMDRAZ21haWwuY29tIiwiaWF0IjoxNjc2NzQ4ODM0LCJleHAiOjE2NzY4MzUyMzR9.P1gy6R6s40UBfB3LHKvEr_0hppzE7U0qlVb7tVFcieg'}`;
-
 export const updateUser = createAsyncThunk(
   'users/update',
   async (info, thunkAPI) => {
--- src/redux/userPage/userPageSlice.js
+++ src/redux/userPage/userPageSlice.js
@@ -10,8 +10,6 @@ const initialState = {
     userPhotoURL: null,
     birthday: null,
   },
-  // token:
-  //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2YwYWQ0ZGY0MjZlY2UyYTJiNWViMzkiLCJlbWFpbCI6ImtlbGx5c2hpa292YTMwMDRAZ21haWwuY29tIiwiaWF0IjoxNjc2ODQyNTc5LCJleHAiOjE2NzY5Mjg5Nzl9.DSJYAc2BoPN5FiAfoLhXblKTUs3p4QebM0SvYr_7fvo',
   token: null,
   isLoggedIn: true,
   isRefreshing: false,
