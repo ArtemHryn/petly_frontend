738f8aa2fa038af52f4a38474f7dcfd078eeb8be
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2023-02-21 00:33:45 +0200

    add newsReducer to store

--- src/redux/news/news-selectors.js
+++ src/redux/news/news-selectors.js
@@ -3,3 +3,5 @@ export const selectNews = state => state.news.items;
 export const selectIsLoading = state => state.news.isLoading;
 
 export const selectError = state => state.news.error;
+
+export const selectFilter = state => state.filter.search;
