583720052d7cb70a15058915a2b853be12265153
NazarBezpaliuk <nazardxvx@gmail.com>
2023-02-21 18:01:21 +0200

    add-bg

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,22 @@
 import { LoginForm } from 'components/LoginForm';
+import backgroundMobile from 'images/registration/waves_mobile.png';
+import backgroundTablet from 'images/registration/waves_tablet.png';
+import backgroundDesktop from 'images/registration/waves_desktop.png';
 
 import { Container } from 'components/Container/Container';
 
 export const LoginPage = () => {
   return (
-    <Container>
+    <Container
+      backgroundImage={[
+        `url('${backgroundMobile}')`,
+        `url('${backgroundTablet}')`,
+        `url('${backgroundDesktop}')`,
+      ]}
+      backgroundSize="contain"
+      backgroundRepeat="no-repeat"
+      backgroundPosition={['bottom -11% right 50%', 'bottom']}
+    >
       <LoginForm />
     </Container>
   );
