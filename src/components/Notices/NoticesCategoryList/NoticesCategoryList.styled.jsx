da128e1291376a98bc58d2ab692d59aebac8392c
Artem <grynbest@gmail.com>
2023-02-23 17:37:34 +0200

    Update NoticesCategoryList.styled.jsx

--- src/components/Notices/NoticesCategoryList/NoticesCategoryList.styled.jsx
+++ src/components/Notices/NoticesCategoryList/NoticesCategoryList.styled.jsx
@@ -5,7 +5,6 @@ export const CardList = styled(motion.ul)`
   @media screen and (min-width: ${p => p.theme.sizes.tablet}) {
     display: flex;
     flex-wrap: wrap;
-    justify-content: center;
     gap: 32px;
   }
 `;
