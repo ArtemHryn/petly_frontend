7a12173691e815120c9c10f245c56bc6afe2c9aa
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2023-02-24 13:34:11 +0200

    add loader and animation

---
+++ src/components/News/NewsList.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ListLoader } from 'components/common/ListLoader/ListLoader';
+import { NewsItem } from './NewsItem';
+import { List } from './NewsList.styled';
+import { fetchNews } from 'redux/news/news-operations';
+import { selectIsLoading, selectNews, selectFilter } from '../../redux/news/news-selectors';
+
+export const NewsList = () => {
+    const dispatch = useDispatch();
+    const news = useSelector(selectNews);
+    const isLoading = useSelector(selectIsLoading);
+    const search = useSelector(selectFilter);
+  
+    useEffect(() => {
+        const controller = new AbortController();
+        dispatch(fetchNews({
+            search,
+            signal: controller.signal,
+        }));
+        return () => controller.abort();
+    }, [dispatch, search]);
+       
+    return isLoading ? (
+        <ListLoader />
+    ) : ( 
+        <>
+            <List
+                initial={{
+                    y: -70,
+                    opacity: 0.3,
+                }}
+                animate={{
+                    y: 0,
+                    opacity: 1,
+                    transition: {
+                    duration: 0.7,
+                    type: 'cubic-bezier(.49,.99,.82,.98)',
+                    delayChildren: 0.5,
+                    },
+                }}
+            >
+                {news.map(({ _id, title, description, date, url }) => {
+                    return (
+                        <NewsItem
+                            key={_id}
+                            title={title}
+                            description={description}
+                            date={date}
+                            url={url}
+                        />
+                    )
+                })}
+            </List>
+        </>
+    );
+};
+
