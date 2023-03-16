79a69d17f8049eb07155af5dc668a895922575ca
Artem <grynbest@gmail.com>
2023-03-16 02:47:49 +0200

    Update partnersOperations.js

--- src/redux/partners/partnersOperations.js
+++ src/redux/partners/partnersOperations.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://arcane-everglades-20653.herokuapp.com';
-
 export const getSponsors = createAsyncThunk(
   'sponsors/get',
   async (_, thunkAPI) => {
